f536b2582fac6809d43482d3ee7787e7ce36e2e1
thymbahutymba <andrea.stevanato.95@hotmail.it>
2018-09-16 14:39:11 +0200

    Fix parse mode in send message, added /kick and /ban command

--- gdgpisausermanager.py
+++ gdgpisausermanager.py
@@ -1,27 +1,30 @@
 # -*- coding: utf-8 -*-
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, ConversationHandler
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
+from config import Config
 import datetime
 import time
 import os
 import random
 import threading
+import logging
 
-TOKEN = ""
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
 new_users = []
-WAITING_TIMEOUT = 30.0
 
 def welcome(bot, update):
 	for new_user_obj in update.message.new_chat_members:
-		print(update.message.chat.title)
-
 		try:
 			new_user = "@" + new_user_obj['username']
 		except Exception as e:
 			new_user = new_user_obj['first_name']
-		print("Nuovo utente: "+new_user)
-		reply_text = "Benvenut* {} in *{}*! Premi _Confermo_ per dimostrare di non essere un bot.".format(str(new_user), str(update.message.chat.title))
+		logger.info("Nuovo utente: {}".format(new_user))
 
+		reply_text = "Benvenut* {} in <b>{}</b>! Premi <i>Confermo</i> per dimostrare di non essere un bot.".format(new_user, update.message.chat.title)
 		new_users.append(new_user_obj.id)
 
 		keyboard = [[InlineKeyboardButton("Confermo", callback_data=1)]]
@@ -30,10 +33,11 @@ def welcome(bot, update):
 		mexid = bot.send_message(
 			update.message.chat_id,
 			text=reply_text,
-			reply_markup=reply_markup
+			reply_markup=reply_markup,
+			parse_mode = ParseMode.HTML
 			)
-
-		threading.Timer(WAITING_TIMEOUT, timer, [bot, new_user_obj.id, update.message.chat_id, mexid.message_id]).start()
+		
+		threading.Timer(Config.WAITING_TIMEOUT, timer, [bot, new_user_obj.id, update.message.chat_id, mexid.message_id]).start()
 		return
 
 def button_pressed(bot, update):
@@ -47,28 +51,65 @@ def button_pressed(bot, update):
 			user = update.callback_query.from_user.first_name
 		
 		bot.send_message(update.callback_query.message.chat_id, text= str(user) + ", grazie per aver completato la registrazione!")
-	else:
-		print("Utente errato!")
 
 def check_activity(bot, update):
 	bot.send_message(
 		chat_id=update.message.chat_id,
 		text="pong",
 		reply_to_message_id=update.message.message_id)
-		
+
+def is_admin(bot, userid, chatid):
+	return userid in [admin.user.id for admin in bot.get_chat_administrators(chatid)]
+
+def ban_user(bot, update):
+	if update.message.reply_to_message is None:
+		return
+
+	if is_admin(bot, update.message.from_user.id, update.message.chat.id):
+		if not is_admin(bot, update.message.reply_to_message.from_user.id, update.message.chat.id):
+			reply_text = "{} è stato bannato con successo.".format(update.message.reply_to_message.from_user.username)
+			bot.kickChatMember(update.message.chat.id, update.message.reply_to_message.from_user.id)
+			bot.send_message(update.message.chat.id, text=reply_text)
+
+			try:
+				user = "@" + update.message.reply_to_message.from_user.username
+			except Exception as e:
+				user = update.message.reply_to_message.from_user.first_name
+			
+			logger.info("The user {} was banned with success".format(user))
+
+def kick_user(bot, update):
+	if update.message.reply_to_message is None:
+		return
+
+	if is_admin(bot, update.message.from_user.id, update.message.chat.id):
+		if not is_admin(bot, update.message.reply_to_message.from_user.id, update.message.chat.id):		
+			bot.kickChatMember(update.message.chat.id, update.message.reply_to_message.from_user.id)
+			bot.unbanChatMember(update.message.chat.id, update.message.reply_to_message.from_user.id)
+
+			try:
+				user = "@" + update.message.reply_to_message.from_user.username
+			except Exception as e:
+				user = update.message.reply_to_message.from_user.first_name
+			
+			logger.info("The user {} was kicked with success".format(user))
+
 def main():
-	updater = Updater(TOKEN)
+	updater = Updater(Config.TOKEN)
 	dp = updater.dispatcher
 	dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, welcome))
 	dp.add_handler(MessageHandler(Filters.regex('^ping$'), check_activity))
 	dp.add_handler(CallbackQueryHandler(button_pressed))
+	dp.add_handler(CommandHandler('ban', ban_user))
+	dp.add_handler(CommandHandler('kick', kick_user))
+
 	updater.start_polling()
 	updater.idle()
 
 def timer(bot, userid, chatid, mexid):
 	global new_users
 	if userid in new_users:
-		print("Kick")
+		logger.info("User with id: {} auto-removed with success".format(userid))
 		bot.kickChatMember(chatid, userid, until_date=25)
 		bot.deleteMessage(chatid, mexid)
 	return
